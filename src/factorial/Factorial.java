f1eb9c4c02811f28f3a66faf28de6e76c467d270
DANIELCASTELAO\vmontestovar <vmontestovar@danielcastelao.org>
2019-11-29 13:31:07 +0100

    "Commit final de la refactorizacion"

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -4,22 +4,22 @@ public class Factorial {
 
     public static void main(String[] args) {
 
-        int j;
-        int f;
+        int numbertofactorize;
+        int increasenumber;
 
-        j = 8;
+        numbertofactorize = 8;
 
         int i;
-        if (j == 0) {
-            f = 1;
+        if (numbertofactorize == 0) {
+            increasenumber = 1;
         } else {
-            f = 1;
-            for (i = j; i >= 1; i--) {
-                f = f * i;
+            increasenumber = 1;
+            for (i = numbertofactorize; i >= 1; i--) {
+                increasenumber = increasenumber * i;
             }
         }
 
-        System.out.println(f);
+        System.out.println(increasenumber);
 
     }
 
